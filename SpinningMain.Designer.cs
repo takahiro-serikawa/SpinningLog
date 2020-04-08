1c9d3e271308e28bcb1ee604bd9acae4f9653204
takahiro.serikawa <takahiro.serikawa@gmail.com>
2020-04-08 22:43:34 +0900

    basic menu and handlers

--- SpinningMain.Designer.cs
+++ SpinningMain.Designer.cs
@@ -27,17 +27,160 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.FileNewMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.FileOpenMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.FileCloseMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.FileCloseAllMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.FileExportMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.AppExitMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ViewRefreshMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.ViewClearMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.HelpAboutMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// webBrowser1
 			// 
+			this.webBrowser1.AllowNavigation = false;
 			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.Location = new System.Drawing.Point(0, 24);
 			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
 			this.webBrowser1.Name = "webBrowser1";
-			this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+			this.webBrowser1.Size = new System.Drawing.Size(800, 426);
 			this.webBrowser1.TabIndex = 0;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileNewMenu,
+            this.FileOpenMenu,
+            this.FileCloseMenu,
+            this.FileCloseAllMenu,
+            this.FileExportMenu,
+            this.toolStripMenuItem1,
+            this.AppExitMenu});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// FileNewMenu
+			// 
+			this.FileNewMenu.Name = "FileNewMenu";
+			this.FileNewMenu.Size = new System.Drawing.Size(180, 22);
+			this.FileNewMenu.Text = "&New ...";
+			this.FileNewMenu.Click += new System.EventHandler(this.FileNewMenu_Click);
+			// 
+			// FileOpenMenu
+			// 
+			this.FileOpenMenu.Name = "FileOpenMenu";
+			this.FileOpenMenu.Size = new System.Drawing.Size(180, 22);
+			this.FileOpenMenu.Text = "&Open ...";
+			this.FileOpenMenu.Click += new System.EventHandler(this.FileOpenMenu_Click);
+			// 
+			// FileCloseMenu
+			// 
+			this.FileCloseMenu.Name = "FileCloseMenu";
+			this.FileCloseMenu.Size = new System.Drawing.Size(180, 22);
+			this.FileCloseMenu.Text = "&Close";
+			this.FileCloseMenu.Click += new System.EventHandler(this.FileCloseMenu_Click);
+			// 
+			// FileCloseAllMenu
+			// 
+			this.FileCloseAllMenu.Name = "FileCloseAllMenu";
+			this.FileCloseAllMenu.Size = new System.Drawing.Size(180, 22);
+			this.FileCloseAllMenu.Text = "Close All";
+			this.FileCloseAllMenu.Click += new System.EventHandler(this.FileCloseAllMenu_Click);
+			// 
+			// FileExportMenu
+			// 
+			this.FileExportMenu.Name = "FileExportMenu";
+			this.FileExportMenu.Size = new System.Drawing.Size(180, 22);
+			this.FileExportMenu.Text = "Export ...";
+			this.FileExportMenu.Click += new System.EventHandler(this.FileExportMenu_Click);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+			// 
+			// AppExitMenu
+			// 
+			this.AppExitMenu.Name = "AppExitMenu";
+			this.AppExitMenu.Size = new System.Drawing.Size(180, 22);
+			this.AppExitMenu.Text = "e&Xit";
+			this.AppExitMenu.Click += new System.EventHandler(this.AppExitMenu_Click);
+			// 
+			// editToolStripMenuItem
+			// 
+			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+			this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+			this.editToolStripMenuItem.Text = "&Edit";
+			// 
+			// viewToolStripMenuItem
+			// 
+			this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ViewRefreshMenu,
+            this.ViewClearMenu});
+			this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+			this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.viewToolStripMenuItem.Text = "&View";
+			// 
+			// ViewRefreshMenu
+			// 
+			this.ViewRefreshMenu.Name = "ViewRefreshMenu";
+			this.ViewRefreshMenu.Size = new System.Drawing.Size(180, 22);
+			this.ViewRefreshMenu.Text = "&Refresh";
+			this.ViewRefreshMenu.Click += new System.EventHandler(this.ViewRefreshMenu_Click);
+			// 
+			// ViewClearMenu
+			// 
+			this.ViewClearMenu.Name = "ViewClearMenu";
+			this.ViewClearMenu.Size = new System.Drawing.Size(180, 22);
+			this.ViewClearMenu.Text = "&Clear";
+			this.ViewClearMenu.Click += new System.EventHandler(this.ViewClearMenu_Click);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.HelpAboutMenu});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// HelpAboutMenu
+			// 
+			this.HelpAboutMenu.Name = "HelpAboutMenu";
+			this.HelpAboutMenu.Size = new System.Drawing.Size(107, 22);
+			this.HelpAboutMenu.Text = "&About";
+			this.HelpAboutMenu.Click += new System.EventHandler(this.HelpAboutMenu_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
 			// SpinningMain
 			// 
@@ -45,17 +188,40 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.webBrowser1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "SpinningMain";
 			this.Text = "spinnin\' log";
 			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.SpinningMain_DragDrop);
 			this.DragOver += new System.Windows.Forms.DragEventHandler(this.SpinningMain_DragOver);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem FileNewMenu;
+		private System.Windows.Forms.ToolStripMenuItem FileOpenMenu;
+		private System.Windows.Forms.ToolStripMenuItem FileCloseMenu;
+		private System.Windows.Forms.ToolStripMenuItem FileCloseAllMenu;
+		private System.Windows.Forms.ToolStripMenuItem FileExportMenu;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem AppExitMenu;
+		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ViewRefreshMenu;
+		private System.Windows.Forms.ToolStripMenuItem ViewClearMenu;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem HelpAboutMenu;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
 	}
 }
 
--- SpinningMain.cs
+++ SpinningMain.cs
@@ -17,12 +17,77 @@ namespace SpinningLog
 			InitializeComponent();
 		}
 
-		private void SpinningMain_DragDrop(object sender, DragEventArgs e)
+
+		// menu handlers
+
+		private void FileNewMenu_Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start(Application.ExecutablePath);
+		}
+
+		private void FileOpenMenu_Click(object sender, EventArgs e)
+		{
+			//openFileDialog1.FileName = 
+			if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+				AddLogFiles(openFileDialog1.FileNames);
+			}
+		}
+
+		private void FileCloseMenu_Click(object sender, EventArgs e)
+		{
+			throw new Exception("no implement");
+		}
+
+		private void FileCloseAllMenu_Click(object sender, EventArgs e)
+		{
+			throw new Exception("no implement");
+		}
+
+		private void FileExportMenu_Click(object sender, EventArgs e)
+		{
+			throw new Exception("no implement");
+		}
+
+		private void ViewRefreshMenu_Click(object sender, EventArgs e)
 		{
 
 		}
 
+		private void ViewClearMenu_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void HelpAboutMenu_Click(object sender, EventArgs e)
+		{
+			throw new Exception("no implement");
+		}
+
+		private void AppExitMenu_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+
+		// drag and drop
+
+		private void SpinningMain_DragDrop(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.Copy;
+			else
+				e.Effect = DragDropEffects.None;
+		}
+
 		private void SpinningMain_DragOver(object sender, DragEventArgs e)
+		{
+			Cursor.Current = Cursors.WaitCursor;
+			string[] filenames = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			AddLogFiles(filenames);
+		}
+
+
+		void AddLogFiles(string[] files)
 		{
 
 		}
